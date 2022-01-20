189e183c4dc7760088bb31dee8549a0ab7b2cf20
Ongchenwen <ongchen9788@gmail.com>
2022-01-20 14:56:31 -0500

    Update

---
+++ src/Amazon/GroupAverage.java
@@ -0,0 +1,28 @@
+package Amazon;
+
+// TODO: 2022-01-20 Need know the output format 
+public class GroupAverage {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,3,2,3};
+        System.out.println(helper(arr));
+    }
+
+    static int helper(int[] arr){
+        int n = arr.length, ans = Integer.MAX_VALUE,res = 0;
+        int[] pre = new int[n];
+        pre[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = arr[i] + pre[i - 1];
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int leftAv = pre[i]/(i + 1);
+            int rightAv = (pre[n - 1] - pre[i])/(n - i - 1);
+
+            ans = Math.min(ans,(Math.abs(leftAv - rightAv)));
+        }
+
+
+        return ans;
+    }
+}
---
+++ src/Amazon/GroupDivision.java
@@ -0,0 +1,28 @@
+package Amazon;
+
+import java.util.Arrays;
+
+public class GroupDivision {
+    public static void main(String[] args) {
+        int[] arr = new int[]{9,3,5,7,1};
+        System.out.println(helper(arr,2,1));
+    }
+
+    static int helper(int[] skill, int teamSize,int maxDiff){
+        Arrays.sort(skill);
+        int currindex = 0;
+        int result = 0;
+        while(currindex + (teamSize -1) < skill.length){ // Terminating condition - no more windows to check
+            // if current window satisfied
+            if((skill[currindex + teamSize - 1] - skill[currindex]) <= maxDiff){
+                result += 1;
+                currindex = currindex+teamSize;
+            } else {
+                // if current window don't satisfied
+                currindex++;
+            }
+        }//End of while
+
+        return result;
+    }
+}
